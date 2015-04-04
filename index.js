ebcc5d3aa58580464140b5f06c2a3ecfc5702aae
Mattias Petter Johansson <mpj@spotify.com>
2015-04-04 23:50:08 +0200

    wip

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var Q = require('q')
 var _ = require('highland')
 var prop = require('mout/function/prop')
+var inspector = function (name) {
+  return function(x) {
+    console.log('inspecting', name+':', x)
+    return x;
+  }
+}
+var argInspector = function() {
+  console.log('argInspector', arguments)
+}
+
 var EventThing = function(db) {
 
   var state = {}
@@ -12,17 +22,29 @@ var EventThing = function(db) {
       { '$inc': { seq: 1 } },
       {new: true}
     ).then(function(doc) {
-      return doc.seq;
+      return doc.value.seq;
     })
   }
 
   function getCollection() {
     if (!state.whenCollection) {
       var deferred = Q.defer();
-      db.createCollection('eventlog', { capped: true, size: 100000, strict: true }, function(err, result) {
-        if (!err) {
+      db.createCollection('eventlog', { capped: true, size: 100000, strict: true }, function(error, result) {
+        if (!!error) {
+          var alreadyExists =
+            error.code === 48 ||
+            error.message === 'Collection eventlog already exists. Currently in strict mode.';
+          if (alreadyExists) {
+            deferred.resolve();
+          } else {
+            throw error;
+          }
+          // already created
+        } else {
+          // TODO: Multiple pieces of code trying to create?
           var whenInitialEvent = Q.ninvoke(db.collection('eventlog'), 'insert', {
-            initialEvent: true
+            initialEvent: true,
+            _id: 0
           });
           var whenOrdinalCounter = Q.ninvoke(db.collection('counters'), 'insert', {
             _id: "eventlog-ordinal",
@@ -31,13 +53,9 @@ var EventThing = function(db) {
 
           Q.all([whenInitialEvent, whenOrdinalCounter]).then(function() {
             deferred.resolve()
-          }).fail(function(err) {
-            console.log(err)
           })
           .done()
         }
-
-
       })
       state.whenCollection = deferred.promise;
     }
@@ -49,24 +67,41 @@ var EventThing = function(db) {
 
   return {
     subscribe: function(opts) {
+      opts = opts || {}
       opts.conditions = opts.conditions || {}
 
       var out = _()
       getCollection().then(function(coll) {
-        var options = {
-          tailable: true,
-          awaitdata: true,
-          numberOfRetries: -1
-        }
+
         var filter = {}
         if (!!opts.offset)
           filter._id = { '$gte': opts.offset };
-        var str = coll
-          .find(filter, options)
-          .stream()
-        str.resume() // force stream to old mode
-        str.pipe(out);
-      })
+
+        var bodyStream = coll.find(filter).stream();
+        bodyStream.pipe(out, { end: false });
+        var lastEnvelope = null;
+        out.observe().each(function(x) {
+          lastEnvelope = x;
+        })
+
+        bodyStream.on('end', function(x) {
+          if (!!lastEnvelope)
+            filter._id =  {$gt: lastEnvelope._id};
+
+          var options = {
+            tailable: true,
+            awaitdata: true,
+            numberOfRetries: -1
+          }
+          var tailStream = coll
+            .find(filter, options)
+            .stream()
+          tailStream.resume();
+          _(tailStream).pipe(out);
+
+        })
+
+      }).done()
       return out.map(prop('body'));
 
     },
@@ -74,15 +109,19 @@ var EventThing = function(db) {
       // FIXME: This approach will mean that the natrual and ordinal order
       // of events will diverge, and that item 5 might be inserted before item 4.
       // We have to make sure that the subscribe stream compensates for this
-      getCollection().then(function(coll) {
-        getNextSequence('eventlog-ordinal').then(function(ordinal) {
-          coll.insert({
-            _id: ordinal,
-            body: evt
-          });
+      return getCollection()
+        .then(function(coll) {
+          return getNextSequence('eventlog-ordinal')
+            .then(function(ordinal) {
+              var deferred = Q.defer();
+              coll.insert({
+                _id: ordinal,
+                body: evt
+              }, deferred.makeNodeResolver())
+              return  deferred.promise;
+            });
         })
 
-      }).done()
     }
   }
 }
--- test.js
+++ test.js
@@ -8,57 +8,111 @@ var isArray = require('mout/lang/isArray')
 var partial = require('mout/function/partial')
 var _ = require('highland')
 var Q = require('q')
+var inspector = function(x) {
+  console.log('inspecting:', x)
+  return x;
+}
 
-describe('', function() {
+Q.longStackSupport = true;
 
+// Mocha swallows errors
+/*
+process.on('uncaughtException', function (error) {
+  console.log('Uncaught error ----\n', error.stack,'\n-------------------\n');
+})*/
+
+describe('when we have a database', function() {
   var db;
-  var thing;
   beforeEach(function(done) {
     var url = 'mongodb://localhost:27017/test-'+Math.floor(Math.random()*100000);
     MongoClient.connect(url, function(err, x) {
       db = x;
-      thing = EventThing(db)
       done()
     });
   })
 
   afterEach(function(done) {
     db.dropDatabase();
-    db.close();
     done()
   })
 
 
   it('streams', function(done) {
+    var thing = EventThing(db)
     _(thing.subscribe({}))
       .through(await({ hello: 'world'}))
       .pull(done)
+    setTimeout(function() {
+      thing.push({
+        hello: 'world'
+      })
+    }, 200)
 
-    thing.push({
-      hello: 'world'
-    })
   })
 
   it('allows streaming written stuff', function(done) {
-    thing.push({ hello: 0 });
-    thing.push({ hello: 1 });
-    thing.push({ hello: 2 });
-    _(thing.subscribe({})).through(await([
-      { hello: 0 },
-      { hello: 1 },
-      { hello: 2 }
-    ])).pull(done);
+    var thing = EventThing(db)
+    Q.all([
+      thing.push({ hello: 0 }),
+      thing.push({ hello: 1 }),
+      thing.push({ hello: 2 }),
+    ]).then(function() {
+      _(thing.subscribe({})).through(await([
+        { hello: 0 },
+        { hello: 1 },
+        { hello: 2 }
+      ])).pull(done);
+    }).done()
+
   })
 
   it('allows streaming with offset', function(done) {
-    thing.push({ hello: 0 });
-    thing.push({ hello: 1 });
-    thing.push({ hello: 2 });
-    _(thing.subscribe({ offset: 1})).through(await.not([
-      { hello: 0 }
-    ])).each(function() {})
-    setTimeout(done, 100)
+    var thing = EventThing(db)
+    Q.all([
+      thing.push({ hello: 1 }), // Remember dummy event
+      thing.push({ hello: 2 }),
+      thing.push({ hello: 3 })
+    ]).then(function() {
+      _(thing.subscribe({ offset: 1 })).through(await.not([
+        { hello: 0 }
+      ])).each(function() {})
+    })
+    setTimeout(done, 1000)
   })
+
+  it('retains state', function(done) {
+    var thing = EventThing(db)
+    thing.push({ hello: 0 }).then(function() {
+      var thing2 = EventThing(db);
+      _(thing2.subscribe({})).through(await([
+        { hello: 0 }
+      ])).pull(done);
+    })
+
+  })
+
+
+  describe('items with space is in the event-log', function() {
+    beforeEach(function(done) {
+      db.collection('eventlog').insert({ _id: 0, body: { hello: 0 } })
+      db.collection('eventlog').insert({ _id: 1, body: { hello: 1 } })
+      db.collection('eventlog').insert({ _id: 3, body: { hello: 3 } })
+      setTimeout(done, 50)
+    })
+
+    it('streams the unbroken block', function(done) {
+      var thing = EventThing(db)
+      _(thing.subscribe())
+        .through(await([{ hello: 0}, {hello:1}])).pull(done)
+    })
+
+    it('does not stream past space', function(done) {
+      var thing = EventThing(db)
+      _(thing.subscribe()).through(await.not({ _id: 3})).pull(function(){})
+      setTimeout(done, 100)
+    })
+  })
+
 
 
   var awaitGeneric = _.curry(function await(expect, patterns, stream) {
